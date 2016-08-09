9be12899b0cbd015f48e4adf4f7d62116f39a017
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-08-09 14:52:26 +0300

    soap client for sletat

---
+++ SletatPlayground/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SletatPlayground
+{
+	public class Config
+	{
+		public string Login { get; set; }
+
+		public string Password { get; set; }
+	}
+}
---
+++ SletatPlayground/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+using SletatLib.Sletat;
+using Newtonsoft.Json;
+
+namespace SletatPlayground
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var configData = File.ReadAllText("config.json");
+			var config = JsonConvert.DeserializeObject<Config>(configData);
+			var auth = new AuthData
+			{
+				Login = config.Login,
+				Password = config.Password
+			};
+
+			if (!Directory.Exists("output"))
+				Directory.CreateDirectory("output");
+
+			using (var service = new Soap11GateClient())
+			{
+				foreach (var hotelId in args.Select(arg => int.Parse(arg)))
+				{
+					var hotelInfo = service.GetHotelInformation(auth, hotelId, string.Empty, false);
+					var hotelStr = JsonConvert.SerializeObject(hotelInfo);
+					File.WriteAllText(Path.Combine("output", $"{hotelId}.json"), hotelStr);
+				}
+			}
+		}
+	}
+}
